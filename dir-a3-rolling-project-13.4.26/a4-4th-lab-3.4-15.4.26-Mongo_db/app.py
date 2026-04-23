1a8eecac877237bf16220bedda4a783e57198ead
Yuval <yuvaldar@gmail.com>
2026-04-23 14:42:41 +0300

    rollin-project-lab4-all basic work first time - now API - do not load

--- dir-a3-rolling-project-13.4.26/a4-4th-lab-3.4-15.4.26-Mongo_db/app.py
+++ dir-a3-rolling-project-13.4.26/a4-4th-lab-3.4-15.4.26-Mongo_db/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
 
-from db import init_db, get_collection
+from db import init_db
 from errors import errors_bp
-from routes.tasks import tasks_bp, _serialize
+from routes.tasks import tasks_bp
 
 
 app = Flask(__name__)
+
+
 init_db(app)
 app.register_blueprint(tasks_bp)
 app.register_blueprint(errors_bp)
@@ -13,12 +15,7 @@ app.register_blueprint(errors_bp)
 
 @app.route("/", methods=["GET"])
 def index():
-    col = get_collection("tasks")
-    initial_tasks = [_serialize(t) for t in col.find({})]
-    # fetches all documents from a MongoDB collection and serializes them.
-    # converts it to a string so the document can be returned as JSON from a Flask route
-
-    return render_template("index.html", initial_tasks=initial_tasks)
+    return render_template("index.html")
 
 
 if __name__ == "__main__":
